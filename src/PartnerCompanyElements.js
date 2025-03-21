f6786164b0e047d658f7dc673c75779af8995e45
Akaki Khimshiashvili <kakikhimshiashvili@gmail.com>
2025-03-21 13:52:46 +0400

    Card hover updated

--- src/PartnerCompanyElements.js
+++ src/PartnerCompanyElements.js
@@ -4,6 +4,14 @@ export default function PartnerCompanyElements({ partnerCompany }) {
   return (
     <div className="card">
       <img src={partnerCompany.companyLogo} className="card-main-img" alt="" />
+      <div className="onhover-text">
+        <a href={partnerCompany.website} target="blanc">
+          <p>
+            Visit{" "}
+            <span className="onhover-text-span">{partnerCompany.name}</span>
+          </p>
+        </a>
+      </div>
     </div>
   );
 }
